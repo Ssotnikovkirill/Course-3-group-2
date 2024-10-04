aaebf97886e7de5a24e72e88e0cf3228b3e6e4c6
Ssotnikovkirill <kirillgor1217@gmail.com>
2024-10-04 18:15:19 +0300

    Feat(Lesson 4): PracticeA

---
+++ Course/3rd year/Lesson5/PracticeA/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PracticeA;
+
+public abstract class Shape
+{
+    public abstract double Area { get; }
+
+    public abstract double GetArea();
+
+    public abstract double GetPerimeter();
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
