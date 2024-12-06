28578ffa0870e3f2f8e2701f40eb2d987329eafa
Ssotnikovkirill <kirillgor1217@gmail.com>
2024-12-06 17:00:53 +0300

    Feat(Lesson18): PracticeB

--- Course/3rd year/Lesson18/ContactManager/DatabaseHelper.cs
+++ Course/3rd year/Lesson18/ContactManager/DatabaseHelper.cs
@@ -30,4 +30,55 @@ public class DatabaseHelper
             }
         }
     }
+
+    public void AddContact(string name, string phone, string email)
+{
+    using (var connection = new SQLiteConnection($"Data Source={DatabaseFileName};Version=3;"))
+    {
+        connection.Open();
+        string insertQuery = "INSERT INTO Contacts (Name, Phone, Email) VALUES (@name, @phone, @email)";
+        using (var command = new SQLiteCommand(insertQuery, connection))
+        {
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@phone", phone);
+            command.Parameters.AddWithValue("@email", email);
+            command.ExecuteNonQuery();
+        }
+    }
 }
+
+public void DeleteContact(int id)
+{
+    using (var connection = new SQLiteConnection($"Data Source={DatabaseFileName};Version=3;"))
+    {
+        connection.Open();
+        string deleteQuery = "DELETE FROM Contacts WHERE Id = @id";
+        using (var command = new SQLiteCommand(deleteQuery, connection))
+        {
+            command.Parameters.AddWithValue("@id", id);
+            command.ExecuteNonQuery();
+        }
+    }
+}
+
+public void SearchContacts(string name)
+{
+    using (var connection = new SQLiteConnection($"Data Source={DatabaseFileName};Version=3;"))
+    {
+        connection.Open();
+        string searchQuery = "SELECT * FROM Contacts WHERE Name LIKE @name";
+        using (var command = new SQLiteCommand(searchQuery, connection))
+        {
+            command.Parameters.AddWithValue("@name", "%" + name + "%");
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"ID: {reader["Id"]}, Name: {reader["Name"]}, Phone: {reader["Phone"]}, Email: {reader["Email"]}");
+                }
+            }
+        }
+    }
+}
+}
+
